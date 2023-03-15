fe53368c63ee27dc1d185485285f21c9555ac8a1
qdrs <qdrs@proton.me>
2023-03-15 12:47:45 +0800

    basic stub

---
+++ solana/src/lib.rs
@@ -0,0 +1,11 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn succeeds_if(_args: TokenStream, input: TokenStream) -> TokenStream {
+    input
+}
+
+#[proc_macro_attribute]
+pub fn invariant(_args: TokenStream, input: TokenStream) -> TokenStream {
+    input
+}
