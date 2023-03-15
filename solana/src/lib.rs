ddf882ad1a450f2e26c8446d306dc6776164645d
qdrs <qdrs@proton.me>
2023-03-15 16:54:22 +0800

    derive arbitrary

--- solana/src/lib.rs
+++ solana/src/lib.rs
@@ -9,3 +9,8 @@ pub fn succeeds_if(_args: TokenStream, input: TokenStream) -> TokenStream {
 pub fn invariant(_args: TokenStream, input: TokenStream) -> TokenStream {
     input
 }
+
+#[proc_macro_derive(Arbitrary)]
+pub fn derive_arbitrary(item: TokenStream) -> TokenStream {
+    item
+}
